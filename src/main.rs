cd1dc6a9984e748c3a93c30b727e938b311e2625
Daichi Yoshida <daichi.yoshida@hacobu.jp>
2021-01-01 11:32:12 +0900

    :sparkles:計算処理の実装

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,60 @@
 use clap::Clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader, stdin};
+
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>)->i32{
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x)= token.parse::<i32>(){
+                stack.push(x)
+            } else { 
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+
+                let res = match token {
+                    "+" => x+y,
+                    "-" => x-y,
+                    "/"=>x/y,
+                    "%"=> x%y,
+                    _=>panic!("invalid token")
+                };
+                stack.push(res);
+            }
+
+            if self.0{
+                println!("{:?} {:?}",tokens, stack)
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else { 
+            panic!("invalid token")
+        }
+
+    }
+}
+           
 
 #[derive(Clap, Debug)]
 #[clap(
-    name="My RPN program",
-    version="1.0.0",
-    author="Your name",
-    about="Super awesome sample RPN calculator",
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
 )]
 struct Opts {
     /// Sets the level of verbosity
@@ -19,11 +68,29 @@ struct Opts {
 
 fn main() {
     let opts = Opts::parse();
-    match opts.formula_file {
-        Some(file)=>println!("File specified: {}", file),
-        None => println!("No file specified"),
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        run(reader, opts.verbose)
+    } else{
+        let stdin = stdin();
+        let reader= stdin.lock();
+        run(reader, opts.verbose);
+        println!("No file is specified")
+    }
+
+    fn run<R: BufRead>(reader:R, verbose: bool) {
+        let calc = RpnCalculator::new(verbose);
+
+        for line in reader.lines(){
+            let line = line.unwrap();
+            let answer = calc.eval(&line);
+            println!("line: {}",line);
+            println!("answer: {}",answer);
+
+        }
     }
     
-    println!("Is verbosity specified?: {}", opts.verbose)
-
 }
